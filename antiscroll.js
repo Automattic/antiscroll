9b84621ec30b895a692ee4d03282c1247b2d8f22
Misha Koryak <koryak@gmail.com>
2013-08-13 21:50:34 -0400

    remove unneeded jquery object creation from scrollbarSize()

--- antiscroll.js
+++ antiscroll.js
@@ -451,16 +451,16 @@
 
   function scrollbarSize () {
     if (size === undefined) {
-      var div = $(
+      var $div = $(
           '<div class="antiscroll-inner" style="width:50px;height:50px;overflow-y:scroll;'
         + 'position:absolute;top:-200px;left:-200px;"><div style="height:100px;width:100%">'
         + '</div>'
       );
 
-      $('body').append(div);
-      var w1 = $(div).innerWidth();
-      var w2 = $('div', div).innerWidth();
-      $(div).remove();
+      $('body').append($div);
+      var w1 = $div.innerWidth();
+      var w2 = $('div', $div).innerWidth();
+      $div.remove();
 
       size = w1 - w2;
     }
